6fe76c7df5426a41294492838f247b9d05a6718f
sonne <sonnne34@gmil.com>
2022-09-09 15:49:25 +0500

    add Dagger dependency

--- app/src/main/java/com/example/dependencyinjectionstart/example1/Activity.kt
+++ app/src/main/java/com/example/dependencyinjectionstart/example1/Activity.kt
@@ -1,13 +1,9 @@
 package com.example.dependencyinjectionstart.example1
 
-import javax.inject.Inject
 
 class Activity {
 
-    @Inject
-    lateinit var computer: Computer
 
     init {
-        DaggerNewComponent.create().inject(this)
     }
 }
