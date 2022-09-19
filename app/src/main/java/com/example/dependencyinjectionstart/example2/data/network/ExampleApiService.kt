80987109dc58b2fea06000fcb1446f8c6795625c
sonne <sonnne34@gmil.com>
2022-09-19 15:14:23 +0500

    add BindInstance

--- app/src/main/java/com/example/dependencyinjectionstart/example2/data/network/ExampleApiService.kt
+++ app/src/main/java/com/example/dependencyinjectionstart/example2/data/network/ExampleApiService.kt
@@ -1,12 +1,17 @@
 package com.example.dependencyinjectionstart.example2.data.network
 
+import android.content.Context
 import android.util.Log
+import com.example.dependencyinjectionstart.R
 import javax.inject.Inject
 
-class ExampleApiService @Inject constructor() {
+class ExampleApiService @Inject constructor(
+    private val times: String,
+    private val context: Context
+) {
 
     fun method() {
-        Log.d(LOG_TAG, "ExampleApiService")
+        Log.d(LOG_TAG, "ExampleApiService ${context.getString(R.string.app_name)} Times = $times")
     }
 
     companion object {
